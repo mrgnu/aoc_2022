1aeaf5f06bdd99631d35af6e961a3febf5645eb8
mr.gnu.jr@gmail.com <mr.gnu.jr@gmail.com>
2022-12-01 10:27:53 +0100

    [day-1] part 1 and 2

---
+++ src/aoc_2022/day_1.clj
@@ -0,0 +1,49 @@
+(ns aoc-2021.day-1
+  (:require [aoc-2022.utils :as utils])
+  )
+
+(def test-input-1-1
+  [
+   "1000"
+   "2000"
+   "3000"
+   ""
+   "4000"
+   ""
+   "5000"
+   "6000"
+   ""
+   "7000"
+   "8000"
+   "9000"
+   ""
+   "10000"
+   ])
+
+(defn input-1-1 []
+  (->> "resources/day_1_1.txt"
+       utils/per-line-input))
+
+(defn- sum-lines [lines]
+  (apply + (map #(Integer. %) lines)))
+
+(defn- parse-input [lines]
+  (->> lines
+       (partition-by empty?)
+       (filter (comp not empty? first))
+       (map sum-lines)))
+
+(defn day-1-1 []
+  (->> (input-1-1)
+       parse-input
+       (apply max))
+  )
+
+(defn day-1-2 []
+  (->> (input-1-1)
+       parse-input
+       sort
+       reverse
+       (take 3)
+       (apply +))
+  )
---
+++ test/aoc_2022/day_1_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-1-test
+  (:require [clojure.test :refer :all]
+            [aoc-2022.day-1 :refer :all]))
+
+(deftest unit-test-day-1
+  (testing "unit tests day 1"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-1
+  (testing "day 1"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
